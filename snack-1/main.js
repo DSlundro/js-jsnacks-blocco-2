9bd3ced9f1d0b226381ea138613d5de51da602b4
DSlundro <dragan.savic95@outlook.it>
2022-03-31 12:55:25 +0200

    boooh

---
+++ snack-1/main.js
@@ -0,0 +1,36 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+
+
+
+let numeroUtente;
+let somma = 0;
+
+
+for (let i = 0; i < 5; i++){
+
+    numeroUtente = prompt('Inserisci un numero');
+    console.log(numeroUtente[i]);
+
+    somma += Number(numeroUtente[i]);
+
+}
+
+console.log(somma)
+
+
+/* //  while   
+let i = 0;
+while (i < 5){
+
+    numeroUtente = prompt('Inserisci un numero');
+    console.log(numeroUtente);
+
+    somma += Number(numeroUtente[i]);
+    i++
+}
+
+console.log(somma)
+
+ */
